fff7d6cefbe1c146a34cdcca5c1686ca80904f6e
Nacho Ballester <hnballes@outlook.com>
2020-08-10 20:49:22 +0200

    loading the model and making one week prediction only with flights

--- streamlit.py
+++ streamlit.py
@@ -2,59 +2,40 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from statsmodels.tsa.statespace.sarimax import SARIMAX
-
-@st.cache
-def data_load():
+import joblib
+import urllib.request
     
-    url = "https://drive.google.com/file/d/1Mm8oluT401FkocpFvinr852FMPNsQMm1/view?usp=sharing"
-    path = 'https://drive.google.com/uc?export=download&id='+url.split('/')[-2]
-    covid = pd.read_csv(path, parse_dates=["date"])
-    covid_eur = covid[covid['continent']=='Europe']
-    covid_eur['new_cases'] = covid_eur['new_cases'].fillna(0)
-    covid_esp = covid_eur[covid_eur["location"]=="Spain"].reset_index()
-    covid_esp.index = covid_esp.date
-    covid_esp_newcases = covid_esp["new_cases"]
-    covid_esp_newcases = covid_esp_newcases.resample('D').sum()
-    covid_esp["MA3"] = covid_esp.new_cases.rolling(3).mean()
-    covid_esp_ma3 = covid_esp["MA3"]
-    covid_esp_diff3 = covid_esp_newcases - covid_esp_ma3
-    url2 = "https://drive.google.com/file/d/1V19t2WshmMUonqZFfWAAF9X2siHiwmv2/view?usp=sharing"
-    path2 = 'https://drive.google.com/uc?export=download&id='+url2.split('/')[-2]
-    flights_esp = pd.read_csv(path2,parse_dates=["FLT_DATE"],index_col="FLT_DATE")
-    flights_esp_arr = flights_esp["FLT_ARR_1"]
-    flights_esp_arr = flights_esp_arr.resample("1D").sum()
-    return covid_esp_diff3,flights_esp_arr
-
-
-#@st.cache
-def model(covid,arrflights,x):
-    model4 = SARIMAX(covid[3:-12], order=(4, 0, 4),exog=arrflights[2:])
-    results_SARIMAX = model4.fit()
-    prediction= results_SARIMAX.get_forecast(steps=len(x),exog=x)
-    return prediction.predicted_mean
-    
-
+varc1=[1]
 # Create a title, a subheader and let the reader know the data is loading.
 st.title("Coronavirus forecast")
-st.subheader("This is an app for predicting new number of coronavirus cases")
-data_load_state = st.text('Loading data...')
-
-# Load coronavirus cases data and flights data.
-covid_esp_diff3, flights_esp_arr = data_load()
-
-# Notify the reader what to do when data is loaded.
-data_load_state.text('Data loaded')
+st.subheader("This is an app for predicting new number of coronavirus cases during a week")
 
 #select number of days to forecast and create the slider buttons
-days=st.radio("Choose the number of days to forecast:",("1","2","3","4"))
+days=7
 x=[]
 for i in range(1,int(days)+1):
     x1 = st.slider(f'Number of arrival flights day '+str(i), 0, 1000, 600)
     x.append(x1)
-z = np.reshape(x, (-1,1))
-predictions=model(covid_esp_diff3,flights_esp_arr,z)
+flightpredictions = np.reshape(x, (-1,1))
 
-#enseñar predicciones
-st.text("Predicted values for the next "+str(days)+" days")
-st.write(predictions)
-st.line_chart(covid_esp_diff3[3:-12].append(predictions))
+
+#select the values of the 2 diferent variables
+st.subheader("testing policy")
+st.text("0 - no testing policy\n1 - only those who both (a) have symptoms AND (b) meet specific criteria (eg key workers)\n2 - testing of anyone showing Covid-19 symptoms\n3 - open public testing (eg drive through testing available to asymptomatic people")
+
+testing = st.slider('choose the testing policy applied next week:', 0, 3, 1)
+testingpolicy=pd.Series(7*[testing])
+
+st.subheader("Record government policy on contact tracing after a positive diagnosis")
+st.text("0 - no contact tracing\n1 - limited contact tracing; not done for all cases\n2 - comprehensive contact tracing; done for all identified cases")
+
+tracing = st.slider('choose the contact tracing policy applied next week:', 0, 3, 1)
+tracingpolicy=pd.Series(7*[testing])
+
+#Load model and make the predictions
+
+model = joblib.load(urllib.request.urlopen("https://drive.google.com/uc?export=download&id=1NeoAEr1Ksa_6Mwu1mNABZqoQlrlmvdrd"))
+results=model.get_forecast(steps=7,exog=flightpredictions)
+st.title("Forecast for next week")
+st.dataframe(results.predicted_mean)
+st.line_chart(results.predicted_mean)
